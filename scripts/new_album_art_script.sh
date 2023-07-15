2789c5ff0f7fcddc41a0a9766d4be14f8f6397f9
AlwaysAndrew8 <62494603+AlwaysAndrew8@users.noreply.github.com>
2023-07-15 12:22:33 -0700

    Add files via upload

---
+++ scripts/new_album_art_script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+status=$(playerctl status)
+COVER="/tmp/music_cover.jpg"
+# MPD_COVER=	ffmpeg -i "${MUSIC_DIR}/$(mpc current -f %file%)" "${COVER}" -y &> /dev/null
+MUSIC_DIR="$HOME/Downloads/Music"
+
+if [ $status = "Playing" -o $status = "Paused" ]; then
+    url=$(playerctl metadata mpris:artUrl | cut -c 8-)
+		cp $url $COVER
+		if cp $url $COVER; then 
+			echo "ok"
+		else
+			echo "failed"
+		fi
+fi
+echo $COVER
